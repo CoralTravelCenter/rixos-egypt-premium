0bc227448af9b78ca7c54fb8e030065f361d693d
shooore <shooore@gmail.com>
2024-08-06 12:38:36 +0300

    + some js utils

--- site/common/js/usefuls.js
+++ site/common/js/usefuls.js
@@ -1,3 +1,15 @@
+export async function preloadScript(url, cb) {
+    return new Promise(resolve => {
+        const script_el = document.createElement('script');
+        script_el.addEventListener('load', () => {
+            script_el.remove();
+            typeof cb === 'function' && cb();
+            resolve();
+        });
+        script_el.src = url;
+        document.head.append(script_el);
+    });
+}
 
 export async function hostReactAppReady(selector = '#__next > div', timeout = 500) {
     return new Promise(resolve => {
@@ -11,6 +23,40 @@ export async function hostReactAppReady(selector = '#__next > div', timeout = 50
         };
         waiter();
     });
+}
+
+export async function vimeoAutoPlay(observer_options = {}) {
+    const vboxes = document.querySelectorAll('.vimeo-video-box [data-vimeo-vid]');
+    if (vboxes.length) {
+        await preloadScript('https://player.vimeo.com/api/player.js');
+        const io = new IntersectionObserver((entries, observer) => {
+            entries.forEach(entry => {
+                const target = entry.target;
+                if (entry.isIntersecting) {
+                    if (!target['vimeo-player']) {
+                        target['vimeo-player'] = new Vimeo.Player(target, {
+                            id: target.dataset.vimeoVid,
+                            background: 1,
+                            playsinline: 1,
+                            autopause: 0,
+                            title: 0,
+                            byline: 0,
+                            portrait: 0,
+                            autoplay: 1,
+                            muted: 1,
+                        });
+                        target['vimeo-player'].on('play', function () {
+                            this.element.parentElement.classList.add('playback');
+                        });
+                    }
+                    target['vimeo-player'].play();
+                } else {
+                    target['vimeo-player']?.pause();
+                }
+            });
+        }, Object.assign({}, { threshold: .33 }, observer_options));
+        vboxes.forEach(box => io.observe(box));
+    }
 }
 
 export async function waitForSelector(selector, interaval = 500) {
