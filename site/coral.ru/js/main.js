cf2a230370b20c0c856ca49f7b0500fed7f1c95f
shooore <shooore@gmail.com>
2024-08-08 09:50:31 +0300

    visuals x-slider

--- site/coral.ru/js/main.js
+++ site/coral.ru/js/main.js
@@ -1,4 +1,4 @@
-import { hostReactAppReady, vimeoAutoPlay } from "../../common/js/usefuls";
+import { hostReactAppReady, preloadScript, vimeoAutoPlay } from "../../common/js/usefuls";
 
 (async function () {
     await hostReactAppReady();
@@ -7,4 +7,18 @@ import { hostReactAppReady, vimeoAutoPlay } from "../../common/js/usefuls";
         .classList.remove(...'layout-container-limit center'.split(' '));
 
     vimeoAutoPlay();
+
+    // preloadScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/gsap.min.js').then(() => {
+    //     preloadScript('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.5/ScrollTrigger.min.js').then(() => {
+    //         ScrollTrigger.create({
+    //             markers: true,
+    //             pin: true,
+    //             anticipatePin: 1,
+    //             trigger: 'section.nav',
+    //             // start: 'top top',
+    //             // end: 100
+    //         });
+    //     });
+    // });
+
 })();
